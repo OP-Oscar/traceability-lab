1dc7eb44b6dbb6a49877855e8d6e4cdb9b82e9f1
OP-Oscar <oscarpena07@outlook.com>
2023-05-12 18:15:00 -0500

    update

--- public/main.js
+++ public/main.js
@@ -9,7 +9,7 @@ btn.addEventListener('click', clickHandler)
 const moviesContainer = document.querySelector('#movies-container')
 const form = document.querySelector('form')
 
-const baseURL = `http://localhost:4000/api/movies`
+const baseURL = `./api/movies`
 // const baseURL = `${__dirname}/api/movies`
 
 
